ba1a492706e5df83d6f2da3fe0dc827ff8992bc5
Daniel Jackson <email@danieljackson.co.uk>
2020-07-30 22:29:39 +0100

    Published as a VS Code extension: danielgjackson.vscode-maximize-fix See: https://marketplace.visualstudio.com/items?itemName=danielgjackson.vscode-maximize-fix

--- README.md
+++ README.md
@@ -1,12 +1,38 @@
-# VS Code Maximized Fix
+# Visual Studio Code Maximized Window Fix (VS Code Extension)
 
-This code tweaks the VS Code window style to fix an issue where the maximized window is larger than the screen.
+<!-- ![Maximize Window](icon.png) -->
+
+## Features
+
+This code tweaks the VS Code window style to fix an issue where the maximized window is larger than the screen on some computers with mixed monitor DPIs.
 
 See [VS Code issue 85592](https://github.com/microsoft/vscode/issues/85592).
 
+* Command *Maximize Window Fix* (`vscode-maximize-fix.fix`) with default hot-key `Ctrl`+`Alt`+`Shift`+`M`.
+* A small status bar icon to perform the action.
+
 The program simply finds any window title ending with *Visual Studio Code* (or specified on the command-line) and removes the style `WS_CAPTION`.
 
-To use run `vscode-maximize-fix.exe` from:
+If you don't want to use the extension, you can manually run `vscode-maximize-fix.exe`, a ready built version available from: [Releases](https://github.com/danielgjackson/vscode-maximize-fix/releases/tag/1.0).
 
-  * [Releases](https://github.com/danielgjackson/vscode-maximize-fix/releases/tag/1.0)
 
+## Requirements
+
+Only runs (and is required) on Windows.
+
+
+## Extension Settings
+
+None.
+
+
+## Release Notes
+
+See: [Change Log](CHANGELOG.md)
+
+
+## Links
+
+* [GitHub Page: danielgjackson/vscode-maximize-fix](https://github.com/danielgjackson/vscode-maximize-fix)
+* [Visual Studio Marketplace: Maximize Window Fix](https://marketplace.visualstudio.com/items?itemName=danielgjackson.vscode-maximize-fix)
+<!-- vsce package && vsce publish -->
---
+++ extension.js
@@ -0,0 +1,118 @@
+const vscode = require('vscode');
+const os = require('os')
+//const path = require('path');
+const fs = require('fs');
+const child_process = require('child_process');
+
+const title = 'Maximize Window Fix';
+
+const titleSuffix = 'Visual Studio Code';
+
+function spawnProcess(context, suffix) {
+	try {
+		const options = {};
+		
+		// Executable that tweaks VS Code window style to fix the issue
+		const command = context.asAbsolutePath('vscode-maximize-fix.exe');
+		
+		let process;
+		if (fs.existsSync(command)) {
+			process = child_process.spawn(command, [suffix], options);
+		} else {
+			console.error(`${title}: External program not found: ${command}`);
+			return false;
+		}
+
+		process.on('close', (code) => {
+			if (code) {
+				console.error(`${title}: Unexpected exit code: ${code}`);
+				vscode.window.showErrorMessage(`${title}: Unexpected process exit: ${code}`);
+			}
+		});		
+		return true;
+	} catch (e) {
+		console.error(e);
+		return false;
+	}
+}
+
+function runFix(context) {
+	const release = os.release().split('.').map(x => parseInt(x));
+	if (os.platform() !== 'win32') {
+		vscode.window.showWarningMessage(`${title}: This extension can only work on Windows.`);
+	} else if (spawnProcess(context, titleSuffix)) {
+		//vscode.window.showInformationMessage(`${title}: Fixing maximized windows`);
+		statusMessage('Fixing maximized windows...');
+	} else {
+		vscode.window.showErrorMessage(`${title}: Error spawning fix process`);
+	}
+}
+
+let statusBarItem = null;
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	const commandId = 'vscode-maximize-fix.fix';
+	
+	console.log(`${title}: activate()`);
+	
+	const disposable = vscode.commands.registerCommand(commandId, () => {
+		runFix(context);
+	});
+	context.subscriptions.push(disposable);
+	
+	// create a new status bar item that we can now manage
+	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 95);
+	statusBarItem.command = commandId;
+	context.subscriptions.push(statusBarItem);
+	
+	updateStatusBarItem();
+}
+
+let currentStatusMessage = null;
+let currentStatusTimeout = null;
+
+function updateStatusBarItem() {
+	if (!statusBarItem) return;
+	// https://code.visualstudio.com/api/references/icons-in-labels
+	// $(chrome-maximize) $(window)
+	let text = `$(chrome-maximize)`;
+	if (currentStatusMessage) {
+		text = text + ' ' + currentStatusMessage;
+	}
+	statusBarItem.text = text;
+	statusBarItem.tooltip = `Fix Maximized Windows`;
+	statusBarItem.show();
+}
+
+function statusMessage(message) {
+	const timeout = 2000;
+	if (currentStatusTimeout) {
+		clearTimeout(currentStatusTimeout);
+		currentStatusTimeout = null;
+	}
+	if (!statusBarItem) {
+		if (!message) {
+			vscode.window.setStatusBarMessage(message, timeout);
+		}
+		return;
+	}
+	currentStatusMessage = message;
+	updateStatusBarItem();
+	currentStatusTimeout = setTimeout(() => {
+		statusMessage(null);
+	}, timeout);
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {
+	console.log(`${title}: deactivate()`);
+	statusBarItem = null;
+}
+
+module.exports = {
+	activate,
+	deactivate
+}
